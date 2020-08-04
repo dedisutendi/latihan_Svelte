16213ee62a901baee47d2bbbfcf51f961184331a
dedisutendi <62795594+dedisutendi@users.noreply.github.com>
2020-08-04 09:26:12 +0700

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+import BigHeader from './component/BigHeader.svelte';
+import data from './instagram.json';
+import PhotoList from './component/PhotoList.svelte';
+
+	export let hashtag;
+
+</script>
+
+<svelte:head>
+  	<link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.0/css/bulma.min.css" rel="stylesheet" />
+</svelte:head>
+
+<main class="container">
+	<BigHeader hashtag = {hashtag} />
+<input type="text" bind:value={hashtag} />
+<h1>Photos</h1>
+<PhotoList/>
+</main>
